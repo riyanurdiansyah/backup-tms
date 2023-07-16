8c376395e6b11a2982ff8c5c8321f7c54ff941ce
arifnurrhmnn <arifnr.arkademi@gmail.com>
2023-07-16 18:05:08 +0700

    setup simulation credit page

---
+++ app/simulation-credit/page.tsx
@@ -0,0 +1,12 @@
+import HeroBanner from "@/components/HeroBanner";
+import React from "react";
+
+const SimulationCredit = () => {
+  return (
+    <div className="simulation-credit-page-wrapper">
+      <HeroBanner title={"Simulation Credit"} />
+    </div>
+  );
+};
+
+export default SimulationCredit;
