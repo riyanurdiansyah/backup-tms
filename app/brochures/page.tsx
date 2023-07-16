f7f1e00d1bc9da1926ae613f2b49c80eef960916
arifnurrhmnn <arifnr.arkademi@gmail.com>
2023-07-16 18:02:23 +0700

    setup brochures page

---
+++ app/brochures/page.tsx
@@ -0,0 +1,12 @@
+import HeroBanner from "@/components/HeroBanner";
+import React from "react";
+
+const BrochuresPage = () => {
+  return (
+    <div className="brochures-page-wrapper">
+      <HeroBanner title={"Brochures"} />
+    </div>
+  );
+};
+
+export default BrochuresPage;
