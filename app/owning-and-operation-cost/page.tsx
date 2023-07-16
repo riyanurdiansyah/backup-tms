2a9a88c859d0054d99938c1741039eb54bd7de7b
arifnurrhmnn <arifnr.arkademi@gmail.com>
2023-07-16 17:42:49 +0700

    setup owning and opeartion cost page

---
+++ app/owning-and-operation-cost/page.tsx
@@ -0,0 +1,12 @@
+import HeroBanner from "@/components/HeroBanner";
+import React from "react";
+
+const OwningAndOperationCostPage = () => {
+  return (
+    <div className="owning-and-operation-cost-page-warapper">
+      <HeroBanner title={"Owning And Operation Cost"} />
+    </div>
+  );
+};
+
+export default OwningAndOperationCostPage;
