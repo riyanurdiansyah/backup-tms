91a5f0ba7db065010c4a33068bd638e0c582af18
arifnurrhmnn <arifnr.arkademi@gmail.com>
2023-07-22 18:29:30 +0700

    setup primereact & primeicons

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,9 @@
 import Sidebar from "@/components/Sidebar";
 import { BodyWrapper, ContentWrapper, LayoutContainer } from "./Styled";
 import Topbar from "@/components/Topbar";
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css"; //core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 
 export default function AdminLayout({
   children, // will be a page or nested layout
